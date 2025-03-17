95450bfee17bc6735815fce1ee01a3dd75df10fd
Rafael Monteiro <rafael.diogo@ipvc.pt>
2025-03-17 11:02:23 +0000

    add

---
+++ src/main/java/com/Proj2/AlfaCafes/AlfaCafesApplication.java
@@ -0,0 +1,13 @@
+package com.Proj2.AlfaCafes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaCafesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaCafesApplication.class, args);
+	}
+
+}
